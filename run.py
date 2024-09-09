222b818b46a01c96e4bea8ca024ca242df97665e
Rose Gekonge <91277475+RoseGekonge@users.noreply.github.com>
2024-09-09 12:45:36 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from module import db
 from module.admin import admin1
 
 
-#Checks if the run.py file has executed directly and not imported
+#Checks if the run.py file has executed directly and not imported. Comment it out when deploying.
 #-------------------------------#
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
